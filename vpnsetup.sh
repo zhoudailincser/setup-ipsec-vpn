7247b4ca208bcb9faa522eec38af5e3cb8b7a2f8
william_chou <william_chou@yeah.net>
2017-06-17 00:45:04 +0800

    config change

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -24,9 +24,9 @@
 # - All values MUST be placed inside 'single quotes'
 # - DO NOT use these characters within values:  \ " '
 
-YOUR_IPSEC_PSK=''
-YOUR_USERNAME=''
-YOUR_PASSWORD=''
+YOUR_IPSEC_PSK='237237237'
+YOUR_USERNAME='zhoudailinvpn'
+YOUR_PASSWORD='237237237'
 
 # Important notes:   https://git.io/vpnnotes
 # Setup VPN clients: https://git.io/vpnclients
